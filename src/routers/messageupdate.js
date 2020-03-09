139aa0923cb66210f203723d3a9b2f4855cda6a4
Herbert Ssevume <you@example.com>
2020-03-09 20:53:29 +0000

    added verify on update route

--- src/routers/messageupdate.js
+++ src/routers/messageupdate.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const User = require('../model/User')
 const verify = require('./verifyToken')
 
-router.post('/messageupdate', async(req, res)=>{
+router.post('/messageupdate',verify, async(req, res)=>{
 
     const user = await User.updateOne({_id:req.body.id},{$set:{message:req.body.message}})
     if(!user) return res.send('Message not updated')
