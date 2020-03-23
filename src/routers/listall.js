86e6d187970b46534a3b7438e94244b0f8450475
Herbert Ssevume <you@example.com>
2020-03-23 11:27:42 +0000

    listall

--- src/routers/listall.js
+++ src/routers/listall.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const User = require('../model/User')
 const verify = require('./verifyToken')
 
-router.post('/listall', async(req, res)=>{
+router.post('/listall',async(req, res)=>{
     
     const user = await User.find()
     if(!user) return res.send('List of registered use not found')
