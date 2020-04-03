1a55a282ded574cd4956b6a73767aa2715dc9002
Herbert Ssevume <you@example.com>
2020-04-03 22:48:04 +0100

    Added redExp on search  _V6.40

--- src/routers/searchdb.js
+++ src/routers/searchdb.js
@@ -3,8 +3,11 @@ const User = require('../model/User')
 const verify = require('./verifyToken')
 
 router.post('/searchdb',verify, async(req, res)=>{
+    //use regex to search surnames with both lower and upper letter
     
-    const user = await User.find({surname:req.body.surname})
+var regex = new RegExp(["^",req.body.surname , "$"].join(""), "i");
+    
+    const user = await User.find({surname:regex})
     if(!user) return res.send('Name not found')
        res.send(user)
            });
